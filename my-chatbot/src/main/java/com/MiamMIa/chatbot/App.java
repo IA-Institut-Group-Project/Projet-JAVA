5ffbe25366c075b79cb64f41d36404bbd74a5552
kilianMeddas <kilianmeddas94380@gmail.com>
2025-01-24 12:23:00 +0100

    First Commit

---
+++ my-chatbot/src/main/java/com/MiamMIa/chatbot/App.java
@@ -0,0 +1,13 @@
+package com.MiamMIa.chatbot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
